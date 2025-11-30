c3b2a0a7629b96f1be7327a3ed6494d45f9f8ff0
Yara <yaramohammmed11@gmail.com>
2025-12-01 00:19:00 +0200

    Implement SSEComputer for error calculation

--- src/main/java/com/future/parallelkmeansgui/core/SSEComputer.java
+++ src/main/java/com/future/parallelkmeansgui/core/SSEComputer.java
@@ -1,12 +1,31 @@
 package com.future.parallelkmeansgui.core;
 
 import com.future.parallelkmeansgui.model.Cluster;
+import com.future.parallelkmeansgui.model.Point;
+import com.future.parallelkmeansgui.util.DistanceCalculator;
 
 import java.util.List;
 
 public class SSEComputer {
 
-    public static double compute(List<Cluster>clusters) {
-        return 0.0;
+    public static double compute(List<Cluster> clusters) {
+        double totalSSE = 0.0;
+
+        for (Cluster cluster : clusters) {
+            Point centroid = cluster.centroid();
+            List<Point> points = cluster.points();
+
+            if (points == null || points.isEmpty()) {
+                continue;
+            }
+
+            for (Point point : points) {
+                double distance = DistanceCalculator.calculate(point, centroid);
+                totalSSE += (distance * distance);
+            }
+        }
+
+        return totalSSE;
     }
 }
+
