0e9f575f0125d6fdc02746884fef76f6006cc9bb
Abdelrhman Mohsen <abdulrahman_20230317@fci.helwan.edu.eg>
2025-12-03 01:12:44 +0200

    Setup Experiment classes

--- src/main/java/com/future/parallelkmeansgui/model/Experiment.java
+++
@@ -1,6 +0,0 @@
-package com.future.parallelkmeansgui.model;
-
-import java.util.List;
-
-public record Experiment(List<Cluster> clusters, long runtime, double sse) {
-}
---
+++ src/main/java/com/future/parallelkmeansgui/util/DatasetLoader.java
@@ -0,0 +1,12 @@
+package com.future.parallelkmeansgui.util;
+
+import com.future.parallelkmeansgui.model.Point;
+
+import java.util.List;
+
+public class DatasetLoader {
+
+    public static List<Point> load2DDataset(String filePath) {
+        return null;
+    }
+}
