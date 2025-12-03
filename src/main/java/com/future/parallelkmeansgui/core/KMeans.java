d059e905d620ff27a19faed4746270eb6090672f
Yara <yaramohammmed11@gmail.com>
2025-12-03 07:33:00 +0200

    Refactor KMeans base class: Add runKMeansWithRestarts bonus feature and Result return type

--- src/main/java/com/future/parallelkmeansgui/core/KMeans.java
+++ src/main/java/com/future/parallelkmeansgui/core/KMeans.java
@@ -17,13 +17,35 @@ public abstract class KMeans {
 
     protected abstract List<Cluster> fit();
 
+
     public Result runKMeans() {
         RuntimeTimer timer = new RuntimeTimer();
         timer.start();
+
         List<Cluster> clusters = fit();
+
         long runtime = timer.stop();
         double sse = SSEComputer.compute(clusters);
+
         return new Result(clusters, runtime, sse);
     }
 
+    /**
+     * BONUS FEATURE: Multiple Random Restarts.
+     */
+    public Result runKMeansWithRestarts(int trials) {
+        Result bestResult = null;
+        double minSSE = Double.MAX_VALUE;
+
+        for (int i = 0; i < trials; i++) {
+
+            Result currentResult = runKMeans();
+
+            if (currentResult.sse() < minSSE) {
+                minSSE = currentResult.sse();
+                bestResult = currentResult;
+            }
+        }
+        return bestResult;
+    }
 }
