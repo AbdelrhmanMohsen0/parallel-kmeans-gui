4e05b6277b14b8b2a21cc82812fb51e96672f7c7
Yara <yaramohammmed11@gmail.com>
2025-12-03 07:35:39 +0200

    Refactor Sequential

--- src/main/java/com/future/parallelkmeansgui/core/KMeansSequentialImpl.java
+++ src/main/java/com/future/parallelkmeansgui/core/KMeansSequentialImpl.java
@@ -9,19 +9,16 @@ import java.util.List;
 
 public class KMeansSequentialImpl extends KMeans {
 
-    private final List<Point> points;
 
     public KMeansSequentialImpl(KMeansConfig config) {
         super(config);
-        this.points = config.points();
     }
 
-    /**
-     * Standard implementation: Runs the algorithm ONCE.
-     */
     @Override
     protected List<Cluster> fit() {
-        // 1. Initialize Centroids
+
+        List<Point> points = config.points();
+
         List<Point> initialCentroids = RandomCentroidsGenerator.generateKCentroids(points, config.k());
 
         List<Cluster> clusters = new ArrayList<>();
@@ -29,21 +26,17 @@ public class KMeansSequentialImpl extends KMeans {
             clusters.add(new Cluster(centroid, new ArrayList<>()));
         }
 
-        // 2. Main Loop
         for (int i = 0; i < config.maxIterations(); i++) {
 
-            // Assign
             for (Point point : points) {
                 PointAssigner.assignToNearestCluster(point, clusters);
             }
 
-            // Recompute
             List<Cluster> newClusters = new ArrayList<>();
             for (Cluster cluster : clusters) {
                 newClusters.add(CentroidReComputer.reCompute(cluster));
             }
 
-            // Converge
             if (CentroidReComputer.hasClustersConverged(clusters, newClusters, config.tolerance())) {
                 return clusters;
             }
@@ -51,30 +44,10 @@ public class KMeansSequentialImpl extends KMeans {
             clusters = newClusters;
         }
 
-        // Final assignment
         for (Point point : points) {
             PointAssigner.assignToNearestCluster(point, clusters);
         }
 
         return clusters;
-    }
-
-    /**
-     * BONUS FEATURE: Multiple Random Restarts.
-     * Keeps K the same, but tries different random seeds to find the lowest SSE for THAT K.
-     */
-    public List<Cluster> fitWithRestarts(int trials) {
-        List<Cluster> bestClusters = null;
-        double minSSE = Double.MAX_VALUE;
-
-        for (int i = 0; i < trials; i++) {
-            List<Cluster> currentResult = this.fit();
-            double currentSSE = SSEComputer.compute(currentResult);
-            if (currentSSE < minSSE) {
-                minSSE = currentSSE;
-                bestClusters = currentResult;
-            }
-        }
-        return bestClusters;
     }
 }
